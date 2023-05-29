192fb6bc260db2fc8961e31f562616f238df3605
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 11:48:52 -0500

    Renderiza

--- src/componentes/TablaTutoria.jsx
+++ src/componentes/TablaTutoria.jsx
@@ -20,10 +20,43 @@ export const TablaTutoria = () => {
 
     const handleEliminarTutoria = async (codigo) => {
         try {
-          await deleteTutoria(codigo);
-          const nuevasTutorias = tutorias.filter(tutoria => tutoria.id !== codigo);
-          setTutorias(nuevasTutorias);
+            await deleteTutoria(codigo);
+            const nuevasTutorias = tutorias.filter(tutoria => tutoria.id !== codigo);
+            setTutorias(nuevasTutorias);
         } catch (error) {
-          console.error('Error al eliminar la sesión:', error);
+            console.error('Error al eliminar la sesión:', error);
         }
-      };
+    };
+
+    return (
+        <div>
+            <h2>Lista de Tutorías</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID Tutoría</th>
+                        <th scope="col">Estudiante</th>
+                        <th scope="col">Día</th>
+                        <th scope="col">Hora</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tutorias.map(tutoria => {
+                        return (
+                            <tr key={tutoria.id}>
+                                <td>{tutoria.id}</td>
+                                <td>{tutoria.tutor.nombre}</td>
+                                <td>{tutoria.dia}</td>
+                                <td>{tutoria.horarioInicio} - {tutoria.horarioFin}</td>
+                                <td>
+                                    <button onClick={() => handleEliminarTutoria(tutoria.id)}>Eliminar</button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+};
