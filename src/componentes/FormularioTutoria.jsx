45514cf7843edbaf8322336b7fb0d235cdad1086
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 11:43:29 -0500

    Renderiza

--- src/componentes/FormularioTutoria.jsx
+++ src/componentes/FormularioTutoria.jsx
@@ -50,54 +50,93 @@ export const FormularioTutoria = ({ agendarTutoria }) => {
         setHorariosSeleccionados(horariosActualizados);
     };
 
-    setHorariosSeleccionados(horariosActualizados);
-};
+    const handleAgendarTutoria = async () => {
+        if (!estudianteSeleccionado) {
+            console.error("Selecciona un estudiante");
+            return;
+        }
 
-const handleAgendarTutoria = async () => {
-    if (!estudianteSeleccionado) {
-        console.error("Selecciona un estudiante");
-        return;
-    }
+        if (horariosSeleccionados.length === 0) {
+            console.error("Selecciona al menos un horario");
+            return;
+        }
 
-    if (horariosSeleccionados.length === 0) {
-        console.error("Selecciona al menos un horario");
-        return;
-    }
+        const estudiante = estudiantes.find((estudiante) => estudiante.id === parseInt(estudianteSeleccionado));
 
-    const estudiante = estudiantes.find((estudiante) => estudiante.id === parseInt(estudianteSeleccionado));
+        if (!estudiante) {
+            console.error("Estudiante no encontrado");
+            return;
+        }
 
-    if (!estudiante) {
-        console.error("Estudiante no encontrado");
-        return;
-    }
+        const horariosSeleccionadosObj = estudiante.disponibilidad.filter((disponibilidad) =>
+            horariosSeleccionados.some(h => h.dia === disponibilidad.dia && h.horario === disponibilidad.horario)
+        );
 
-    const horariosSeleccionadosObj = estudiante.disponibilidad.filter((disponibilidad) =>
-        horariosSeleccionados.some(h => h.dia === disponibilidad.dia && h.horario === disponibilidad.horario)
-    );
+        const tutoria = {
+            tutor: {
+                codigo: estudiante.id,
+                nombre: estudiante.nombre,
+                facultad: estudiante.facultad,
+                programa: estudiante.programa,
+                semestre: estudiante.semestre,
+                habilidades: estudiante.habilidades,
+                disponibilidad: horariosSeleccionadosObj,
+            },
+            dia: horariosSeleccionadosObj[0].dia,
+            horarioInicio: horariosSeleccionadosObj[0].horario.split(" - ")[0],
+            horarioFin: horariosSeleccionadosObj[0].horario.split(" - ")[1],
+        };
 
-    const tutoria = {
-        tutor: {
-            codigo: estudiante.id,
-            nombre: estudiante.nombre,
-            facultad: estudiante.facultad,
-            programa: estudiante.programa,
-            semestre: estudiante.semestre,
-            habilidades: estudiante.habilidades,
-            disponibilidad: horariosSeleccionadosObj,
-        },
-        dia: horariosSeleccionadosObj[0].dia,
-        horarioInicio: horariosSeleccionadosObj[0].horario.split(" - ")[0],
-        horarioFin: horariosSeleccionadosObj[0].horario.split(" - ")[1],
+        try {
+            const nuevaTutoria = await postTutorias(tutoria);
+            console.log("Tutoría registrada:", nuevaTutoria);
+            agendarTutoria(nuevaTutoria);
+            setEstudianteSeleccionado("");
+            setDisponibilidadEstudiante([]);
+            setHorariosSeleccionados([]);
+        } catch (error) {
+            console.error("Error al registrar la tutoría:", error);
+        }
     };
 
-    try {
-        const nuevaTutoria = await postTutorias(tutoria);
-        console.log("Tutoría registrada:", nuevaTutoria);
-        agendarTutoria(nuevaTutoria);
-        setEstudianteSeleccionado("");
-        setDisponibilidadEstudiante([]);
-        setHorariosSeleccionados([]);
-    } catch (error) {
-        console.error("Error al registrar la tutoría:", error);
-    }
+    return (
+        <div>
+            <h2>Registro de Tutorías</h2>
+            <div>
+                <label>Estudiante:</label>
+                <select value={estudianteSeleccionado} onChange={handleEstudianteChange}>
+                    <option value="">Selecciona un estudiante</option>
+                    {estudiantes.map((estudiante) => (
+                        <option key={estudiante.id} value={estudiante.id}>
+                            {estudiante.nombre}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <h3>Horarios Disponibles:</h3>
+                {estudianteSeleccionado && disponibilidadEstudiante.length > 0 ? (
+                    <div>
+                        {disponibilidadEstudiante.map((disponibilidad, index) => (
+                            <div key={index}>
+                                <input
+                                    type="checkbox"
+                                    id={JSON.stringify(disponibilidad)}
+                                    value={JSON.stringify(disponibilidad)}
+                                    checked={horariosSeleccionados.some(h => h.dia === disponibilidad.dia && h.horario === disponibilidad.horario)}
+                                    onChange={handleHorarioChange}
+                                />
+                                <label htmlFor={JSON.stringify(disponibilidad)}>{`${disponibilidad.dia} - ${disponibilidad.horario}`}</label>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>No hay disponibilidad disponible</p>
+                )}
+            </div>
+            <div>
+                <button onClick={handleAgendarTutoria}>Agendar Tutoría</button>
+            </div>
+        </div>
+    );
 };
