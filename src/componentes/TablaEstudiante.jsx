fa7257fc0f1157288d7d4b5b85e4bf1035b91413
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 11:11:25 -0500

    Obtiene la lista de estudiantes

--- src/componentes/TablaEstudiante.jsx
+++ src/componentes/TablaEstudiante.jsx
@@ -3,5 +3,18 @@ import { getEstudiantes } from "../peticiones/getEstudiantes";
 import { deleteEstudiantes } from "../peticiones/deleteEstudiantes";
 
 export const TablaEstudiante = ({ eliminarEstudiante }) => {
-  const [estudiantes, setEstudiantes] = useState([]);
-  const [mensajeEliminacion, setMensajeEliminacion] = useState("");
+    const [estudiantes, setEstudiantes] = useState([]);
+    const [mensajeEliminacion, setMensajeEliminacion] = useState("");
+
+    useEffect(() => {
+        const fetchEstudiantes = async () => {
+            try {
+                const estudiantesData = await getEstudiantes();
+                setEstudiantes(estudiantesData);
+            } catch (error) {
+                console.error("Error al obtener los estudiantes:", error);
+            }
+        };
+
+        fetchEstudiantes();
+    }, []);
