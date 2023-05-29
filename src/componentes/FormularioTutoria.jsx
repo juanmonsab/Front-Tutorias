cd57b42715d781c14528ff46a5dab05b51b6b3b0
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 11:27:43 -0500

    Obtiene la lista de estudiantes al cargar el componente

--- src/componentes/FormularioTutoria.jsx
+++ src/componentes/FormularioTutoria.jsx
@@ -3,9 +3,49 @@ import { getEstudiantes } from "../peticiones/getEstudiantes";
 import { postTutorias } from "../peticiones/postTutorias";
 
 export const FormularioTutoria = ({ agendarTutoria }) => {
-  const [estudiantes, setEstudiantes] = useState([]);
-  const [estudianteSeleccionado, setEstudianteSeleccionado] = useState("");
-  const [disponibilidadEstudiante, setDisponibilidadEstudiante] = useState([]);
-  const [horariosSeleccionados, setHorariosSeleccionados] = useState([]);
+    const [estudiantes, setEstudiantes] = useState([]);
+    const [estudianteSeleccionado, setEstudianteSeleccionado] = useState("");
+    const [disponibilidadEstudiante, setDisponibilidadEstudiante] = useState([]);
+    const [horariosSeleccionados, setHorariosSeleccionados] = useState([]);
 
-  
+    useEffect(() => {
+        const fetchEstudiantes = async () => {
+            try {
+                const estudiantesData = await getEstudiantes();
+                setEstudiantes(estudiantesData);
+            } catch (error) {
+                console.error("Error al obtener los estudiantes:", error);
+            }
+        };
+
+        fetchEstudiantes();
+    }, []);
+
+    const handleEstudianteChange = (event) => {
+        const estudianteCodigo = event.target.value;
+        setEstudianteSeleccionado(estudianteCodigo);
+        const estudiante = estudiantes.find((estudiante) => estudiante.id === parseInt(estudianteCodigo));
+        if (estudiante) {
+            setDisponibilidadEstudiante(estudiante.disponibilidad);
+            setHorariosSeleccionados([]);
+        } else {
+            setDisponibilidadEstudiante([]);
+            setHorariosSeleccionados([]);
+        }
+    };
+
+    const handleHorarioChange = (event) => {
+        const horario = JSON.parse(event.target.value);
+        const horariosActualizados = [...horariosSeleccionados];
+
+        if (event.target.checked) {
+            horariosActualizados.push(horario);
+        } else {
+            const index = horariosActualizados.findIndex(h => h.dia === horario.dia && h.horario === horario.horario);
+            if (index !== -1) {
+                horariosActualizados.splice(index, 1);
+            }
+        }
+
+        setHorariosSeleccionados(horariosActualizados);
+    };
