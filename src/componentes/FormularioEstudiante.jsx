835f46e00a807056360f248e952042d3a3d96590
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 10:59:35 -0500

    Creación del objeto estudiante con los datos del formulario

--- src/componentes/FormularioEstudiante.jsx
+++ src/componentes/FormularioEstudiante.jsx
@@ -2,25 +2,34 @@ import React, { useState } from "react";
 import { postEstudiantes } from "../peticiones/postEstudiantes";
 
 export const FormularioEstudiante = ({ agregarEstudiante }) => {
-  const [nombre, setNombre] = useState("");
-  const [semestre, setSemestre] = useState("");
-  const [facultad, setFacultad] = useState("");
-  const [habilidades, setHabilidades] = useState("");
-  const [diasDisponibles, setDiasDisponibles] = useState([]);
-  const [nuevoDia, setNuevoDia] = useState("");
-  const [horaInicio, setHoraInicio] = useState("");
-  const [horaFin, setHoraFin] = useState("");
-  const [programa, setPrograma] = useState("");
+    const [nombre, setNombre] = useState("");
+    const [semestre, setSemestre] = useState("");
+    const [facultad, setFacultad] = useState("");
+    const [habilidades, setHabilidades] = useState("");
+    const [diasDisponibles, setDiasDisponibles] = useState([]);
+    const [nuevoDia, setNuevoDia] = useState("");
+    const [horaInicio, setHoraInicio] = useState("");
+    const [horaFin, setHoraFin] = useState("");
+    const [programa, setPrograma] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-    if (nombre.length < 3) {
-        alert("El nombre debe tener al menos 3 caracteres.");
-        return;
-      }
-  
-      if (diasDisponibles.length === 0) {
-        alert("Debe agregar al menos una disponibilidad.");
-        return;
-      }
+        if (nombre.length < 3) {
+            alert("El nombre debe tener al menos 3 caracteres.");
+            return;
+        }
+
+        if (diasDisponibles.length === 0) {
+            alert("Debe agregar al menos una disponibilidad.");
+            return;
+        }
+
+        let estudiante = {
+            nombre: nombre,
+            semestre: semestre,
+            facultad: facultad,
+            habilidades: habilidades.split(",").map((habilidad) => habilidad.trim()),
+            disponibilidad: diasDisponibles,
+            programa: programa,
+        };
