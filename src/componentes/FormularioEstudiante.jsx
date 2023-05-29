c1a45fc4fd9f15c6c24c2140f58d87dff872dd6f
Juan Montes Sabogal <montessabogaljuan@icloud.com>
2023-05-29 10:56:59 -0500

    Declaración de los estados utilizando useState

---
+++ src/componentes/FormularioEstudiante.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { postEstudiantes } from "../peticiones/postEstudiantes";
+
+export const FormularioEstudiante = ({ agregarEstudiante }) => {
+  const [nombre, setNombre] = useState("");
+  const [semestre, setSemestre] = useState("");
+  const [facultad, setFacultad] = useState("");
+  const [habilidades, setHabilidades] = useState("");
+  const [diasDisponibles, setDiasDisponibles] = useState([]);
+  const [nuevoDia, setNuevoDia] = useState("");
+  const [horaInicio, setHoraInicio] = useState("");
+  const [horaFin, setHoraFin] = useState("");
+  const [programa, setPrograma] = useState("");
